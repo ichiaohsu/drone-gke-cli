c3e613dae319564d1a5655a926ae85166fa29c9c
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-03-07 21:04:47 +0800

    Add namespace for plugin

--- check-service.sh
+++ check-service.sh
@@ -4,18 +4,25 @@ if [ -z ${PLUGIN_NAMESPACE} ]; then
   PLUGIN_NAMESPACE="default"
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_CLUSTER} ]; then
-  KUBERNETES_CLUSTER=$PLUGIN_KUBERNETES_CLUSTER
+if [ ! -z ${PLUGIN_CLUSTER} ]; then
+  KUBERNETES_CLUSTER=$PLUGIN_CLUSTER
 fi
 
 if [ ! -z ${GOOGLE_CREDENTIALS} ]; then
-  echo -e "$GOOGLE_CREDENTIALS" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' > /tmp/gcloud.json
+  echo "$GOOGLE_CREDENTIALS" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' > /tmp/gcloud.json
 fi
+
+if [ ! -z ${PLUGING_ZONE} ]; then
+  GCP_ZONE=$PLUGING_ZONE
+fi
+
+ls /tmp
+cat /tmp/gcloud.json
 
 SERVICE_NAME=$DRONE_REPO-$DRONE_REPO_BRANCH
 echo $SERVICE_NAME
 
-gcloud auth activate-service-account --keyfile /tmp/gcloud.json
+gcloud auth activate-service-account --key-file /tmp/gcloud.json
 gcloud container clusters get-credentials ${KUBERNETES_CLUSTER}
 
 EXTERNAL_IP=""
@@ -24,5 +31,5 @@ while [ -z $EXTERNAL_IP ]; do
   EXTERNAL_IP=$(kubectl get svc ${SERVICE_NAME} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$EXTERNAL_IP" ] && sleep 10
 done
-echo 'End point ready:' && echo $EXTERNAL_IP
+echo 'End point ready:' && echo $external_ip
 
