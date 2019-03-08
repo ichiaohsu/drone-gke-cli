ecba5c5a4e7b1877865076efa37e407bc2b28ba5
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-03-08 11:36:23 +0800

    Debugged credentials

--- check-service.sh
+++ check-service.sh
@@ -8,28 +8,35 @@ if [ ! -z ${PLUGIN_CLUSTER} ]; then
   KUBERNETES_CLUSTER=$PLUGIN_CLUSTER
 fi
 
-if [ ! -z ${GOOGLE_CREDENTIALS} ]; then
-  echo "$GOOGLE_CREDENTIALS" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' > /tmp/gcloud.json
+CREDENTIAL_PATH=./gcloud.json
+# if [ ! -z ${GOOGLE_CREDENTIALS} ]; then
+echo "$GOOGLE_CREDENTIALS" > $CREDENTIAL_PATH
+  # echo "$GOOGLE_CREDENTIALS" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' > /tmp/gcloud.json
+# fi
+
+PLUGING_ZONE=asia-east1-a
+
+if [ ! -z ${PLUGIN_ZONE} ]; then
+  GCP_ZONE=$PLUGIN_ZONE
 fi
 
-if [ ! -z ${PLUGING_ZONE} ]; then
-  GCP_ZONE=$PLUGING_ZONE
-fi
+cat $CREDENTIAL_PATH
 
-ls /tmp
-cat /tmp/gcloud.json
+SERVICE_NAME=${DRONE_REPO}-${DRONE_REPO_BRANCH}
+echo "service name:${SERVICE_NAME}"
+echo "namespace:${PLUGIN_NAMESPACE}"
+echo "zone:${GCP_ZONE}"
+echo "cluster:${KUBERNETES_CLUSTER}"
 
-SERVICE_NAME=$DRONE_REPO-$DRONE_REPO_BRANCH
-echo $SERVICE_NAME
-
-gcloud auth activate-service-account --key-file /tmp/gcloud.json
-gcloud container clusters get-credentials ${KUBERNETES_CLUSTER}
+gcloud auth activate-service-account --key-file $CREDENTIAL_PATH
+echo "gcloud container clusters --zone ${GCP_ZONE} get-credentials ${KUBERNETES_CLUSTER}"
+gcloud container clusters --zone ${GCP_ZONE} get-credentials ${KUBERNETES_CLUSTER}
 
 EXTERNAL_IP=""
 while [ -z $EXTERNAL_IP ]; do
   echo "Waiting for end point..."
-  EXTERNAL_IP=$(kubectl get svc ${SERVICE_NAME} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  EXTERNAL_IP=$(kubectl -n ${PLUGIN_NAMESPACE} get service ${SERVICE_NAME} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$EXTERNAL_IP" ] && sleep 10
 done
-echo 'End point ready:' && echo $external_ip
 
+echo 'End point ready:' && echo $EXTERNAL_IP
