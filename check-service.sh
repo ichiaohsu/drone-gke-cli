db1303e53fbe7aeccbf499518d0a97999c1543e2
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-03-13 17:28:34 +0800

    Fix error when creating account info, and removed unnecessary logs

--- check-service.sh
+++ check-service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -z ${PLUGIN_NAMESPACE} ]; then
   PLUGIN_NAMESPACE="default"
 fi
@@ -8,11 +7,9 @@ if [ ! -z ${PLUGIN_CLUSTER} ]; then
   KUBERNETES_CLUSTER=$PLUGIN_CLUSTER
 fi
 
+# Create info from Environment Variables
 CREDENTIAL_PATH=./gcloud.json
-if [ ! -z ${GOOGLE_CREDENTIALS} ]; then
-  echo "$GOOGLE_CREDENTIALS" > $CREDENTIAL_PATH
-  # echo "$GOOGLE_CREDENTIALS" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' > /tmp/gcloud.json
-fi
+echo "$GOOGLE_CREDENTIALS" > $CREDENTIAL_PATH
 
 PLUGING_ZONE=asia-east1-a
 
@@ -21,13 +18,8 @@ if [ ! -z ${PLUGIN_ZONE} ]; then
 fi
 
 SERVICE_NAME=${DRONE_REPO_NAME}-${DRONE_BRANCH}
-echo "service name:${SERVICE_NAME}"
-echo "namespace:${PLUGIN_NAMESPACE}"
-echo "zone:${GCP_ZONE}"
-echo "cluster:${KUBERNETES_CLUSTER}"
 
 gcloud auth activate-service-account --key-file $CREDENTIAL_PATH
-# echo "gcloud container clusters --zone ${GCP_ZONE} get-credentials ${KUBERNETES_CLUSTER}"
 gcloud container clusters --zone ${GCP_ZONE} get-credentials ${KUBERNETES_CLUSTER}
 
 EXTERNAL_IP=""
@@ -36,7 +28,5 @@ while [ -z $EXTERNAL_IP ]; do
   EXTERNAL_IP=$(kubectl -n ${PLUGIN_NAMESPACE} get service ${SERVICE_NAME} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$EXTERNAL_IP" ] && sleep 10
 done
-
-echo 'End point ready:' && echo $EXTERNAL_IP
 
 echo http://$EXTERNAL_IP > .address
